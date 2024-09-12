57657027a7f5f8df2d076b2fc478a3757a3fc06a
ShadmanAhm3d <shadman01010@gmail.com>
2024-09-12 18:08:39 +0530

    first

---
+++ src/components/Balance.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export const Balance = ({ value }) => {
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/v1/account/balance", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        setBalance(response.data.balance);
+      })
+      .catch((error) => {
+        console.error("Error fetching balance:", error);
+      });
+  }, []); // Empty array as dependency
+
+  return (
+    <div className="flex">
+      <div className="font-bold text-lg">Your balance</div>
+      <div className="font-semibold ml-4 text-lg">Rs {balance}</div>
+    </div>
+  );
+};
+
